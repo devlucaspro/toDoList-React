30d08cdcd2c65bfd2b0e8484ea520c57cc11750e
Lucas Ribeiro <lucasribeiro.130605@gmail.com>
2023-05-24 11:33:21 -0300

    new design

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from '../pages/home';
+import Home from '../pages/Home';
 import Register from '../pages/Register';
 import Admin from '../pages/Admin';
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600
-  }
+  plugins: [react()]
 })
